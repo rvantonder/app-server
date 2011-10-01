e5b4f0a2463f2047bba0da98908d9603fe025a04
Rai <rvantonder@gmail.com>
2011-10-01 07:52:47 -0500

    setting up unit test

--- test_conn.py
+++ test_conn.py
@@ -5,7 +5,7 @@ from string import *
 if __name__ == '__main__':
   username = "0123456789"
   conn = httplib.HTTPConnection('127.0.0.1:3000')
-  conn.request("GET", "/UCRequest?userid="+username)
+  conn.request("GET", "/codeRequest?userid="+username)
   r1 = conn.getresponse()
   if r1.status == 404:
     print "404"
